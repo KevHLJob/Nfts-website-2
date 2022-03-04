72e16ebb40109b9387176f07cf6daaf6624cf3d3
KevHLJob <KevHLJob@github.com>
2022-03-03 20:47:56 -0600

    section home and popular

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,13 @@
 let menu= document.querySelector('.menu-icon');
 let nav=document.querySelector('.menu');
+let notification= document.querySelector('.notification');
 menu.onclick=()=>{
     nav.classList.toggle('active');
     menu.classList.toggle('move');
+    notification.classList.remove('active');
+
+}
+
+document.querySelector('#bell-icon').onclick =()=>{
+    notification.classList.toggle('active');
 }
